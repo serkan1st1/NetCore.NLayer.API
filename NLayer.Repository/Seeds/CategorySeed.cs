9acee50429f6d3072827bdd5a8092e49bcbe7159
Serkan Topal <serkan98topal@gmail.com>
2023-10-08 00:11:48 +0300

    Repository layer completed

---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(new Category { Id = 1, Name = "Teknoloji" });
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id=1,
+                CategoryId=1,
+                Name="Samsung Telefon",
+                Price=100,
+                Stock=20,
+                CreatedDate=DateTime.Now,
+            });
+        }
+    }
+}
